fa13db33c299b1d93254bc5be78bfd4eb08457b3
SayedM00 <93325951+SayedM00@users.noreply.github.com>
2025-09-09 18:39:20 +0400

    Following the login button flow chart

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,3 @@
 // app/api/auth/[...nextauth]/route.js
-import { handlers } from "@/app/[locale]/auth";
-
+import { handlers } from "@/app/_libs/auth"; // Referring to the auth.ts we just created
 export const { GET, POST } = handlers;
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [new URL("https://lh3.googleusercontent.com/a/**")],
+    domains: ["lh3.googleusercontent.com"],
+  },
+};
 
 export default withNextIntl(nextConfig);
