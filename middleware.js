e40b62fa7ac8944e78daada075d1b32e3b75b432
SayedM00 <93325951+SayedM00@users.noreply.github.com>
2025-12-03 22:44:52 +0400

    re-write middleware

--- middleware.js
+++ middleware.js
@@ -1,13 +1,9 @@
-import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
-import { auth } from "./app/_libs/auth";
+import createMiddleware from 'next-intl/middleware';
+import { routing } from './i18n/routing';
 
-export default auth((req) => {
-  return createMiddleware(routing)(req);
-});
+export default createMiddleware(routing);
 
 export const config = {
-  matcher: [
-    "/((?!api|_next|_vercel|.*\\..*).*)"
-  ],
+
+  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
 };
